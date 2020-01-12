7d2ecd1313fc2a2389d50bbad175181f485cef4f
Tisha Holder <50963507+TishaHolder@users.noreply.github.com>
2020-01-12 18:52:14 -0500

    modified sendgrid

--- utils/sendgrid.js
+++ utils/sendgrid.js
@@ -8,9 +8,9 @@ exports.twilioNotification = (email, activeUserFirstName, activeUserLastName, sp
   
     const msg = {
       to: email,
-      from: 'notifications@SplittheBillApp',
+      from: 'notifications@SplitTheBillApp',
       subject: `${activeUserFirstName} sent you a bill notification from SplitTheBillApp`,
-      text: `${activeUserFirstName} has sent you a bill notification from <a href='https://www.splitthebillmain.netlify.com/'>SplittheBillMain.netlify.com</a>`,
+      text: `${activeUserFirstName} has sent you a bill notification from <a href='https://www.splitthebillmain.netlify.com/'>SplitTheBillMain.netlify.com</a>`,
       html: `You owe <strong style="color:#5557FE;">&nbsp;${activeUserFirstName} ${activeUserLastName}&nbsp;</strong> $${split_each_amount} for ${description} on ${created_at}.`
     };
     sgMail.send(msg);
