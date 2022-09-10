216e09115094c89bc4b91d30cbfccc0686d36f86
Tisha <tisha_h20@yahoo.com>
2022-09-10 15:03:02 -0400

    updates to cors policy

--- server.js
+++ server.js
@@ -18,6 +18,12 @@ server.get('/', (req, res) => {
   res.send(
     `Welcome to Split The Bill!`,
   );
+
+  res.setHeader("Access-Control-Allow-Origin", "*")
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Max-Age", "1800");
+  res.setHeader("Access-Control-Allow-Headers", "content-type");
+  res.setHeader( "Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS" ); 
 });
 
 server.use('/api/users', UsersRouter);
