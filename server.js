fad402931fccd8f95a6b90219c29a6102d02a267
Tisha <tisha_h20@yahoo.com>
2022-09-10 14:51:54 -0400

    updates to cors policy

--- server.js
+++ server.js
@@ -12,14 +12,6 @@ const server = express();
 server.use(Requestlogger); //custom logging middleware for incoming requests
 server.use(express.json());
 server.use(helmet());
-
-const corsOptions ={
-  origin:'http://localhost:3000', 
-  credentials:true,            //access-control-allow-credentials:true
-  optionSuccessStatus:200
-}
-
-app.use(cors(corsOptions));
 server.use(cors());
 
 server.get('/', (req, res) => {
