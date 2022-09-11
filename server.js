34383713c719ad2e7343a66ad94ae6068ef4d3c9
Tisha <tisha_h20@yahoo.com>
2022-09-11 07:44:42 -0400

    cors error updates

--- server.js
+++ server.js
@@ -12,12 +12,25 @@ const server = express();
 server.use(Requestlogger); //custom logging middleware for incoming requests
 server.use(express.json());
 server.use(helmet());
-server.use(cors());
+//server.use(cors());
 
 server.get('/', (req, res) => {
   res.send(
     `Welcome to Split The Bill!`,
   );
+});
+
+server.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
 });
 
 server.use('/api/users', UsersRouter);
