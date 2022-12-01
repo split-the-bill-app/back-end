d1bafb9e624e07cd8914d27786abc4f260650590
Tisha <tisha_h20@yahoo.com>
2022-12-01 09:54:28 -0500

    updates

--- api/notifications/notification-router.js
+++ api/notifications/notification-router.js
@@ -24,41 +24,32 @@ router.get('/', AuthMiddleware.restricted, async (req, res) => {
 });
 
 //ADD A NEW NOTIFICATION ARRAY OF EMAILS
-router.post(
-  '/', 
-  AuthMiddleware.restricted,
-  ValidateMiddleware.validateNotification,
-  async (req, res) => {
+router.post('/', AuthMiddleware.restricted, ValidateMiddleware.validateNotification, async (req, res) => {
   try {
     let { bill_id, email } = req.body;
 
     let createdNotifications = [];
     let billForNotification = null;
 
-    if (bill_id && email && Object.keys(req.body).length == 2 && Array.isArray(email) ){                     
-      //find bill for the bill_id entered as part of req.body
-      //notifications are sent for one bill at a time    
+    if (bill_id && email && Object.keys(req.body).length == 2 && Array.isArray(email) ){                    
       await Bills.findById(bill_id)
       .then((billForNotificationFound) => {
         if(billForNotificationFound){
           billForNotification = {...billForNotificationFound};
         }
       })
-      .catch( error => {
-          console.log('billForNotificationFound error', error);
-      })
+      .catch(error => {
+          console.log('No bill found for created notifications.', error);
+      })       
 
-      console.log('billForNotification', billForNotification);  
-
-      //first we create and add the notifications to the database
+      //first create and add the notifications to the database
       email.forEach(email => {
         Notification.add({ bill_id, email })        
         .then(id => {//returns an object with the id ---> { id: 9 } 
           if(id){
             Notification.findById(id.id)                
             .then(newNotification => {         
-              if(newNotification && billForNotification){  
-                console.log('if new notification and bill for notification', newNotification);              
+              if(newNotification && billForNotification){                              
                 createdNotifications.push({
                   id: newNotification.id,
                   bill_id: newNotification.bill_id,
@@ -66,43 +57,40 @@ router.post(
                   split_each_amount: billForNotification.split_each_amount,
                   description: billForNotification.description ? billForNotification.description : '',
                   created_at: billForNotification.created_at
-                }); 
-                console.log('created notifications 1', createdNotifications);  
+                });                
               }            
             })
+            .catch(error => {     
+              res.status(500).json({
+                error: 'An error occurred while adding the notification(s) to the database.',
+              });
+            }); 
             
           }else{
             console.log('No id returned after adding a new notification.');
           }                     
         })
-        .then( async (result) => {
-          const activeUser = await Users.findById(billForNotification.user_id);
-          
-          console.log('result', result);
-          console.log('created notifications 2', createdNotifications);
-      
-          //find notifications for the bill id
-          //and send a twilio notification for each of them
-          //await Bills.findBillNotifications(bill_id) 
-          //.then(awaitbillNotifications => {
-            if(activeUser){
-              console.log('created notifications 3', createdNotifications);
-              createdNotifications.forEach(notification => {          
-                goSend.twilioNotification(
-                  notification.email,
-                  activeUser.firstname,
-                  activeUser.lastname,
-                  notification.split_each_amount,
-                  notification.description,
-                  notification.created_at
-                );
-              })
-            }        
-
+        //then create twilio notification(s)
+        .then( async () => {
+          const activeUser = await Users.findById(billForNotification.user_id);         
+             
+          if(activeUser){
+            console.log('created notifications 3', createdNotifications);
+            createdNotifications.forEach(notification => {          
+              goSend.twilioNotification(
+                notification.email,
+                activeUser.firstname,
+                activeUser.lastname,
+                notification.split_each_amount,
+                notification.description,
+                notification.created_at
+              );
+            })
+          }        
         })
         .catch(error => {     
           res.status(500).json({
-            error: 'An error occurred while sending the notification(s).',
+            error: 'An error occurred while sending the notification(s) via Twilio.'
           });
         });
 
@@ -111,57 +99,20 @@ router.post(
         message: 'Notification(s) sent successfully.',
       });
 
-    } else {
+    }else {
       res.status(400).json({
         warning:
           'Not all information were provided to create a new notification.',
       });
-    }//end else  
+    }//end else     
     
-    //find bill for the bill_id entered as part of req.body
-    //notifications are sent for one bill at a time
-    //const billForNotification = await Bills.findById(bill_id);
-
-    // Create twilio notification
-    //const activeUser = await Users.findById(billForNotification.user_id);
-
-    //console.log('created notifications 2', createdNotifications);
-
-    //find notifications for the bill id
-    //and send a twilio notification for each of them
-    //await Bills.findBillNotifications(bill_id) 
-    //.then(awaitbillNotifications => {
-      // if(activeUser){
-      //   console.log('created notifications 3', createdNotifications);
-      //   createdNotifications.forEach(notification => {          
-      //     goSend.twilioNotification(
-      //       notification.email,
-      //       activeUser.firstname,
-      //       activeUser.lastname,
-      //       notification.split_each_amount,
-      //       notification.description,
-      //       notification.created_at
-      //     );
-      //   })
-      // }        
-    //})
-    //.catch(error => {       
-      //res.status(500).json({
-       // error:
-          //'An error occurred while sending twilio notifications 1!'
-      //})
-    //}) 
-
-    }//end outer try
-    catch(error){
-      res.status(500).json({
-        error:
-          'An error occurred while sending twilio notifications 2!'
-      })
-    }   
-    
-  }//end endpoint
-);//end router.post
+  }//end try
+  catch(error){
+    res.status(500).json({      
+      error: 'A server error occurred while sending the notification(s).'
+    })
+  }     
+});//end router.post
 
 
 //UPDATE A NOTIFICATION
